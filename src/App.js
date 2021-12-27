5c3e8ea70de9eb3e3995c1272ff121d007a9edba
Ryan Diercks <ryanjdiercks@gmail.com>
2021-12-27 10:20:52 -0500

    Fix error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import StressTest from './pages/Stress';
-import FabricGrid from './pages/FabricGrid';
+// import FabricGrid from './pages/FabricGrid';
 import CanvasComponent from './pages/collision';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<StressTest />} />
-          <Route exact path="/grid" element={<FabricGrid />} />
+          {/* <Route exact path="/grid" element={<FabricGrid />} /> */}
           <Route exact path="/collision" element={<CanvasComponent />} />
         </Routes>
       </BrowserRouter>
